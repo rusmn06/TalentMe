dbfdda061a707558f5b9ffcc0925b89e0c3ff848
ARaihanRidha <2210817110001@mhs.ulm.ac.id>
2024-12-03 23:17:57 +0800

    Menambahkan fitur multi bahasa, dan membenarkan string resource

--- app/src/main/java/com/example/talentme/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/talentme/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.talentme.ui.notifications
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +9,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.talentme.R
 import com.example.talentme.databinding.FragmentNotificationsBinding
+import java.util.Locale
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -33,5 +34,40 @@ class NotificationsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding?.setLanguageBtn?.setOnClickListener {
+            val languages = arrayOf("Indonesia", "English")
+            val selectedLanguageIndex = if (getSelectedLanguage() == "id") 0 else 1
+
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle(getString(R.string.choose_language))  // Gunakan string resource jika ada
+                .setSingleChoiceItems(languages, selectedLanguageIndex) { dialog, which ->
+                    when (which) {
+                        0 -> setAppLanguage("id")
+                        1 -> setAppLanguage("en")
+                    }
+                    dialog.dismiss()
+                }
+                .show()
+        }
+    }
+    private fun setAppLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+        requireActivity().resources.updateConfiguration(config, requireActivity().resources.displayMetrics)
+
+        val sharedPreferences = requireActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("selected_language", languageCode).apply()
+
+        requireActivity().recreate()
+    }
+    private fun getSelectedLanguage(): String {
+        val sharedPreferences = requireActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("selected_language", "en") ?: "en"
     }
 }
